c225cd4d54d0cd13500ee1eb07427156dfe4cd1d
Jun Hyuk Han <hanjun.1998@gmail.com>
2022-04-02 17:05:17 +0900

    basic working

--- fdf.c
+++ fdf.c
@@ -16,6 +16,13 @@ typedef struct	s_map
 	int		h_diff;
 	int		w_diff;
 
+	int		camera_x_offset;
+	int		camera_y_offset;
+
+	float		angle_x;
+	float		angle_y;
+	float		angle_z;
+
 	void	*mlx;
 	void	*win;
 
@@ -62,8 +69,8 @@ void	check_map(t_map *m, int fd)
 		max = m->width;
 	else
 		max = m->height;
-	m->w_diff = (WIN_WIDTH - 100) / max;
-	m->h_diff = (WIN_HEIGHT - 100) / max;
+	m->w_diff = (WIN_WIDTH - 400) / max;
+	m->h_diff = (WIN_HEIGHT- 400) / max;
 }
 
 void	parse_map(t_map *m, int fd)
@@ -86,7 +93,7 @@ void	parse_map(t_map *m, int fd)
 		while (*split_res)
 		{
 			temp = ft_atoi(*split_res);
-			m->map[i++].z = temp;
+			m->map[i++].z = temp * 2;
 			split_res++;
 		}
 		ft_free_all(cpy);
@@ -95,15 +102,17 @@ void	parse_map(t_map *m, int fd)
 	}
 }
 
-void	ft_mlx_pixel_put(t_map *m, int x, int y)
+void	ft_mlx_pixel_put(t_map *m, int x, int y, int color)
 {
 	char	*dst;
+	x += m->camera_x_offset;
+	y += m->camera_y_offset;
 
 	dst = m->addr + (y * m->line_length + x * (m->bpp / 8));
-	*(unsigned int*)dst = 0xFFFFFFFF;
+	*(unsigned int*)dst = color;
 }
 
-void draw_line(t_map *m, int x0, int y0, int x1, int y1)
+void draw_line(t_map *m, t_coord a, t_coord b, int color)
 {
 	int	dx;
 	int	dy;
@@ -112,36 +121,36 @@ void draw_line(t_map *m, int x0, int y0, int x1, int y1)
 	int	err;
 	int	e2;
 
-	dx = abs(x1 - x0);
-	dy = -abs(y1 - y0);
-	if (x0 < x1)
+	dx = abs(b.x - a.x);
+	dy = -abs(b.y - a.y);
+	if (a.x < b.x)
 		sx = 1;
 	else
 		sx = -1;
-	if (y0 < y1)
+	if (a.y < b.y)
 		sy = 1;
 	else
 		sy = -1;
 	err = dx + dy;
 	while (1)
 	{
-		ft_mlx_pixel_put(m, x0, y0);
-		if (x0 == x1 && y0 == y1)
+		ft_mlx_pixel_put(m, a.x, a.y, color);
+		if (a.x == b.x && a.y == b.y)
 			break ;
 		e2 = 2 * err;
 		if (e2 >= dy)
 		{
-			if (x0 == x1)
+			if (a.x == b.x)
 				break ;
 			err += dy;
-			x0 += sx;
+			a.x += sx;
 		}
 		if (e2 <= dx)
 		{
-			if (y0 == y1)
+			if (a.y == b.y)
 				break ;
 			err += dx;
-			y0 += sy;
+			a.y += sy;
 		}
 	}
 
@@ -150,7 +159,7 @@ void draw_line(t_map *m, int x0, int y0, int x1, int y1)
 //   int err = (dx>dy ? dx : -dy)/2, e2;
 
 //   for(;;){
-//     ft_mlx_pixel_put(m, x0,y0);   //color the pixel in the matrix for later use.
+//     ft_mlx_pixel_put(m, x0,y0, color);   //color the pixel in the matrix for later use.
 //     if (x0==x1 && y0==y1) break;
 //     e2 = err;
 //     if (e2 >-dx) { err -= dy; x0 += sx; }
@@ -158,7 +167,7 @@ void draw_line(t_map *m, int x0, int y0, int x1, int y1)
 //   }
 }
 
-// void	draw_line(t_map *m, int a, int b, int c, int d)
+// void	draw_line(t_map *m, int a, int b, int c, int d, color)
 // {
 // 	int	dx;
 // 	int	dy;
@@ -173,7 +182,7 @@ void draw_line(t_map *m, int x0, int y0, int x1, int y1)
 // 	y = b;
 // 	while (x <= c)
 // 	{
-// 		ft_mlx_pixel_put(m, x, y);
+// 		ft_mlx_pixel_put(m, x, y, color);
 // 		if (D > 0)
 // 		{
 // 			y++;
@@ -196,18 +205,72 @@ void iso(int *x, int *y, int z)
     *y = -z + (previous_x + previous_y) * sin(0.523599);
 }
 
-void	draw(t_map *m)
+void	set_coord(t_map *m)
 {
 	int	i;
 	int	j;
 	int	idx;
 
-	m->mlx = mlx_init();
-	m->win = mlx_new_window(m->mlx, WIN_WIDTH, WIN_HEIGHT, "fdf");
-	m->img = mlx_new_image(m->mlx, WIN_WIDTH, WIN_HEIGHT);
-	m->addr = mlx_get_data_addr(m->img, &m->bpp, &m->line_length, &m->endian);
+	i = 0;
+	// printf("w_diff:%d h_diff:%d\n", m->w_diff, m->h_diff);
+	while (i < m->height)
+	{
+		j = 0;
+		while (j < m->width)
+		{
+			idx = i * m->width + j;
+			m->map[idx].x = m->w_diff * j;
+			m->map[idx].y = m->h_diff * i;
+			j++;
+		}
+		i++;
+	}
+}
 
-	printf("w: %d h: %d\n", m->width, m->height);
+void	rotate_x(int *y, int *z, int angle)
+{
+	int	prev_y;
+
+	prev_y = *y;
+	*y = prev_y * cos(angle) + *z * sin(angle);
+	*z = -prev_y * sin(angle) + *z * cos(angle);
+}
+
+void	rotate_y(int *x, int *z, int angle)
+{
+	int	prev_x;
+
+	prev_x = *x;
+	*x = prev_x * cos(angle) + *z * sin(angle);
+	*z = -prev_x * sin(angle) + *z * cos(angle);
+}
+
+void	rotate_z(int *x, int *y, int angle)
+{
+	int	prev_x;
+	int	prev_y;
+
+	prev_x = *x;
+	prev_y = *y;
+	*x = prev_x * cos(angle) - prev_y * sin(angle);
+	*y = prev_x * sin(angle) + prev_y * cos(angle);
+}
+
+t_coord	project(t_map *m, t_coord coord)
+{
+
+	iso(&coord.x, &coord.y, coord.z);
+	rotate_x(&coord.y, &coord.z, m->angle_x);
+	rotate_y(&coord.x, &coord.z, m->angle_y);
+	rotate_z(&coord.x, &coord.y, m->angle_z);
+	return (coord);
+}
+
+void	draw_coord(t_map *m, int color)
+{
+	int	i;
+	int	j;
+	int	idx;
 
 	i = -1;
 	while (++i < m->height)
@@ -216,40 +279,83 @@ void	draw(t_map *m)
 		while (++j < m->width)
 		{
 			idx = i * m->width + j;
-			printf("(%d,%d),", m->map[idx].x, m->map[idx].y);
+			//printf("(%d,%d),", m->map[idx].x, m->map[idx].y);
 			if (j > 0)
-				draw_line(m, m->map[idx - 1].x, m->map[idx - 1].y, m->map[idx].x, m->map[idx].y);
+				draw_line(m, project(m, m->map[idx - 1]), project(m, m->map[idx]), color);
+
 			if (i > 0)
-				draw_line(m, m->map[idx - m->width].x, m->map[idx - m->width].y, m->map[idx].x, m->map[idx].y);
+				draw_line(m, project(m, m->map[idx - m->width]), project(m, m->map[idx]), color);
 		}
-		printf("\n");
 	}
-	mlx_put_image_to_window(m->mlx, m->win, m->img, 0, 0);
-	mlx_loop(m->mlx);
 }
 
-void	set_coord(t_map *m)
+int	render_frame(t_map *m)
 {
-	int	i;
-	int	j;
-	int	idx;
+	ft_bzero(m->addr, WIN_WIDTH * WIN_HEIGHT * (m->bpp / 8));
+	draw_coord(m, 0xFFFFFFFF);
+	mlx_put_image_to_window(m->mlx, m->win, m->img, 0, 0);
+	return (0);
+}
 
-	i = 0;
-	printf("w_diff:%d h_diff:%d\n", m->w_diff, m->h_diff);
-	while (i < m->height)
+int	handle_key(int key, t_map *m)
+{
+	printf("(%d)\n", key);
+	switch (key)
 	{
-		j = 0;
-		while (j < m->width)
-		{
-
-			idx = i * m->width + j;
-			m->map[idx].x = m->w_diff * j;
-			m->map[idx].y = m->h_diff * i;
-			iso(&m->map[idx].x, &m->map[idx].y, m->map[idx].z);
-			j++;
-		}
-		i++;
+		case 53:
+			exit(0);
+			break;
+		case 13:
+			m->camera_y_offset -= 10;
+			break;
+		case 0:
+			m->camera_x_offset -= 10;
+			break;
+		case 1:
+			m->camera_y_offset += 10;
+			break;
+		case 2:
+			m->camera_x_offset += 10;
+			break;
+		case 123:
+			m->angle_x += 0.5;
+			break;
+		case 124:
+			m->angle_x -= 0.5;
+			break;
+		case 126:
+			m->angle_y += 0.5;
+			break;
+		case 125:
+			m->angle_y -= 0.5;
+			break;
+		case 12:
+			m->angle_z += 0.5;
+			break;
+		case 14:
+			m->angle_z -= 0.5;
+			break;
 	}
+	render_frame(m);
+	return (0);
+}
+
+void	draw(t_map *m)
+{
+	m->mlx = mlx_init();
+	m->win = mlx_new_window(m->mlx, WIN_WIDTH, WIN_HEIGHT, "fdf");
+	m->img = mlx_new_image(m->mlx, WIN_WIDTH, WIN_HEIGHT);
+	m->addr = mlx_get_data_addr(m->img, &m->bpp, &m->line_length, &m->endian);
+	m->camera_x_offset = (m->width / 2) * m->w_diff;
+	m->camera_y_offset = (m->height / 2) * m->h_diff;
+	m->angle_x = 0;
+	m->angle_y = 0;
+	m->angle_z = 0;
+
+	render_frame(m);
+	//mlx_key_hook(m->win, handle_key, m);
+	mlx_hook(m->win, 2, 0, handle_key, m);
+	//mlx_loop_hook(m->mlx, render_frame, m);
 }
 
 int	main(int argc, char **argv)
@@ -277,5 +383,6 @@ int	main(int argc, char **argv)
 	printf("w: %d h: %d\n", m.width, m.height);
 	set_coord(&m);
 	draw(&m);
+	mlx_loop(m.mlx);
 	return (0);
 }
--- fdf.h
+++ fdf.h
@@ -2,7 +2,7 @@
 # define FDF_H
 
 # define BUFFER_SIZE 30
-# define WIN_WIDTH 800
+# define WIN_WIDTH 1200
 # define WIN_HEIGHT 800
 # include "get_next_line.h"
 # include "libft/libft.h"
